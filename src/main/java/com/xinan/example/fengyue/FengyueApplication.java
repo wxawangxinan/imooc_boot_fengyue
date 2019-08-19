b62c2d53ab24c063f38057f4fdb4f98a75a7ce5c
wangxinan <1994006433@qq.com>
2019-08-19 14:44:08 +0800

    first commit

---
+++ src/main/java/com/xinan/example/fengyue/FengyueApplication.java
@@ -0,0 +1,13 @@
+package com.xinan.example.fengyue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FengyueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FengyueApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/xinan/example/fengyue/util/PropertiesConfig.java
@@ -0,0 +1,29 @@
+package com.xinan.example.fengyue.util;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ConfigurationProperties(prefix = "com.imooc.user")
+@PropertySource(value = "classpath:resource.properties")
+public class PropertiesConfig {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
